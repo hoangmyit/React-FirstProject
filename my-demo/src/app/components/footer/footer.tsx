3b60ee01eb588c867b2ee8fc4c26be80eb7d6d83
My Hoang <nobuyuki.dangki@gmail.com>
2021-03-20 05:58:04 +0700

    add footer css

--- my-demo/src/app/components/footer/footer.tsx
+++ my-demo/src/app/components/footer/footer.tsx
@@ -1,31 +1,34 @@
 import * as React from 'react';
 import Github from '../../custom-icons/icons/github';
 import Linkedin from '../../custom-icons/icons/linkedin';
+import Mail from '../../custom-icons/icons/mail';
 import Skype from '../../custom-icons/icons/skype';
+import './footer.scss';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="social-container">
+      <div className="icon-container">
         <span className="footer-icon">
-          <Github size={50} />
+          <a href="mailto:hoangmyit@outlook.com">
+            <Mail size={36} />
+          </a>
         </span>
         <span className="footer-icon">
-          <Link href="https://www.linkedin.com/in/hoang-my/" target="_blank">
-            <Linkedin size={50} />
-          </Link >
+          <a href="skype:live:c0243f097c9ba312?chat">
+            <Skype size={36} />
+          </a>
         </span>
         <span className="footer-icon">
-          <Skype size={50} />
+          <a href="https://github.com/hoangmyit" target="_blank" rel="noreferrer">
+            <Github size={36} />
+          </a>
         </span>
-        <span className="footer-icon" />
-        <span className="footer-icon" />
-      </div>
-      <div className="topic-container">
-        <div className="topic-item">as</div>
-        <div className="topic-item">as</div>
-        <div className="topic-item">as</div>
-        <div className="topic-item">as</div>
+        <span className="footer-icon">
+          <a href="https://www.linkedin.com/in/hoang-my/" target="_blank" rel="noreferrer">
+            <Linkedin size={36} />
+          </a>
+        </span>
       </div>
       <div className="signature-container">Hoang My &copy; 2021</div>
     </footer>
--- my-demo/src/index.tsx
+++ my-demo/src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bulma/css/bulma.min.css';
 import './index.scss';
 import App from './app/App';
 import reportWebVitals from './reportWebVitals';
-import 'bulma/css/bulma.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
