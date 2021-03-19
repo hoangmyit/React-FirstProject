c2f156af24881965f6f0dcdfcdc88f6b0459693f
My Hoang <nobuyuki.dangki@gmail.com>
2021-03-20 03:45:59 +0700

    add skype icon

--- my-demo/src/app/components/footer/footer.tsx
+++ my-demo/src/app/components/footer/footer.tsx
@@ -1,20 +1,25 @@
 import * as React from 'react';
 import Github from '../../custom-icons/icons/github';
 import Linkedin from '../../custom-icons/icons/linkedin';
+import Skype from '../../custom-icons/icons/skype';
 
 function Footer() {
   return (
     <footer className="footer">
       <div className="social-container">
-        <span className="icon">
-          <Linkedin />
+        <span className="footer-icon">
+          <Github size={50} />
         </span>
-        <span className="icon">
-          <Github />
+        <span className="footer-icon">
+          <Link href="https://www.linkedin.com/in/hoang-my/" target="_blank">
+            <Linkedin size={50} />
+          </Link >
         </span>
-        <span className="icon" />
-        <span className="icon" />
-        <span className="icon" />
+        <span className="footer-icon">
+          <Skype size={50} />
+        </span>
+        <span className="footer-icon" />
+        <span className="footer-icon" />
       </div>
       <div className="topic-container">
         <div className="topic-item">as</div>
